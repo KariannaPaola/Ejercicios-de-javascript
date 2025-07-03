92f105f08d72f15d8b12790976c9a9bb2238a80f
KariannaPaola <kariannaarriaga07@gmail.com>
2025-07-03 12:21:49 -0700

    Update ejercicios.js

--- ejercicios.js
+++ ejercicios.js
@@ -1,16 +1,16 @@
 /*Crea una funcion recursiva que basado en un array de notas, te muestre el promedio*/
 
-let ArregloNotas=[20,19,16,20,18,19]
+let arregloNotas=[18,18,20,16,18,17]
 
-function promedioNotas(ArregloNotas, numero, acumulador=0){
-  if(numero<ArregloNotas.length){
-    acumulador= acumulador + ArregloNotas[numero];
-    resultado= acumulador/ArregloNotas.length;
-    if(numero===ArregloNotas.length-1){
+function promedioNotas(notas, numero, acumulador=0){
+  if(numero<notas.length){
+    acumulador= acumulador + notas[numero];
+    resultado= acumulador/notas.length;
+    if(numero===notas.length-1){
       console.log("El promedio de notas es " + resultado);
     }  
-  promedioNotas(ArregloNotas, numero + 1, acumulador);
+  promedioNotas(notas, numero + 1, acumulador);
   }
 }
-promedioNotas(ArregloNotas,0,0)
+promedioNotas(arregloNotas,0,0)
 
